dc59805f1390999f65d578a893a3ff0fcf785de8
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2023-08-07 00:38:03 +0700

    Change Links

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -29,7 +29,7 @@ function formatTime(timestamp) {
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+            <Link v-if="$page.props.auth.user" :href="route('posts.index')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
             Dashboard</Link>
 
